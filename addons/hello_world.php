34f9d812639c1586f5cd957334da3e51dc9b707f
Rui Mendes <Rui Mendes>
2016-04-26 01:40:05 +0100

    Default Package

--- addons/hello_world.php
+++ addons/hello_world.php
@@ -3,20 +3,19 @@
 /* EXAMPLE HOW TO USE ADDON AS A PUBLIC FUNCTION AND ADMIN FUNCTION */
 
 // LANG
-//addLang('wyname', 'What is your name');
 $l['wyname'] = 'What is your name';
 $l['focus'] = isset($l['focus']) ? $l['focus'].',hi' : ',hi';
 
-function admin_hi() {
+function admin_hello() {
 	echo 'Everything is fine. Cool';
 }
 
-function public_hi() {
+function public_hello() {
 	if ($_POST) {
 		if ($_POST['name']) {echo '<h1>Hello '.clean($_POST['name']).'</h1>';}
 	} else {
 		echo '<h2>Hello world</h2>';
-		echo html_input('form', '', 'post', '', '', '', '', '', '', '', '', '', 'post', _SITE.'hi', '')."\r\n";
+		echo html_input('form', '', 'post', '', '', '', '', '', '', '', '', '', 'post', _SITE.'hello', '')."\r\n";
 		echo html_input('text', 'name', 'name', '', '* '.l('wyname'), 'text', '', '', '', '', '', '', '', '', '')."\r\n";
 		echo html_input('hidden', 'addon', 'addon', 'hi', '', '', '', '', '', '', '', '', '', '', '');
 		echo html_input('submit', 'submit', 'submit', l('submit'), '', 'button', '', '', '', '', '', '', '', '', '')."\r\n";
@@ -25,7 +24,7 @@ function public_hi() {
 }
 
 
-// http://localhost/snews1.80/hi/
-// http://localhost/snews1.80/?action=admin_hi
+// http://localhost/snews1.80/hello/
+// http://localhost/snews1.80/?action=admin_hello
 
 ?>
